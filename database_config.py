630a912284432c3179229f8d1cf215f940a6f387
Santhosh kumar <devsandy12@gmail.com>
2024-09-26 11:44:12 +0530

    db config to prod

--- database_config.py
+++ database_config.py
@@ -1,13 +1,13 @@
 db_config = {
     'host': 'localhost',
-    'user': 'root',
-    'password': '',
+    'user': 'wealthi_admin',
+    'password': 'Wealthi@#123',
     'database': 'wealthi'
 }
 
 db_config_hastar = {
     'host': 'localhost',
-    'user': 'root',
-    'password': '',
+    'user': 'hastar',
+    'password': 'Hastar@#123',
     'database': 'hastar'
 }
